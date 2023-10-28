9d4ffd675256bd2d2a2cb475f0d0b408145a48e8
penekhun <penekhun@gmail.com>
2023-10-29 01:04:11 +0900

    feat: SpringSecurity FilterChain 초기 작성

---
+++ src/main/java/org/penekhun/wanted2023/global/security/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package org.penekhun.wanted2023.global.security.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.penekhun.wanted2023.global.security.filter.JwtFilterApplier;
+import org.penekhun.wanted2023.global.security.provider.JwtTokenProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  public final JwtTokenProvider tokenProvider;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        // 세션을 사용하지 않음.
+        .sessionManagement(sessionManagement -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // CSRF 토큰을 사용하지 않음.
+        .csrf(AbstractHttpConfigurer::disable)
+        // 기본 로그인 페이지를 사용하지 않음.
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+
+        .authorizeHttpRequests(req -> req
+            .anyRequest().permitAll())
+
+        .apply(new JwtFilterApplier(tokenProvider));
+
+    return http.build();
+  }
+}
