8c95e98003b94909af1b3f8ce9a5c826c7f85051
penekhun <penekhun@gmail.com>
2023-10-29 19:45:25 +0900

    feat: Spring Security Method 활성화 (@Secured)

--- src/main/java/org/penekhun/wanted2023/global/security/config/SecurityConfig.java
+++ src/main/java/org/penekhun/wanted2023/global/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.penekhun.wanted2023.global.security.filter.JwtFilterApplier;
 import org.penekhun.wanted2023.global.security.provider.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
 
   public final JwtTokenProvider tokenProvider;
