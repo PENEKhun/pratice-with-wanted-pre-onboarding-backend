b91353fcd047550c13d3f8bc088d693b732fc318
penekhun <penekhun@gmail.com>
2023-10-30 04:34:53 +0900

    feat: @EnableJpaAuditing 활성화하는 Configuration 생성

---
+++ src/main/java/org/penekhun/wanted2023/global/security/config/JpaAuditingApplier.java
@@ -0,0 +1,10 @@
+package org.penekhun.wanted2023.global.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingApplier {
+
+}
