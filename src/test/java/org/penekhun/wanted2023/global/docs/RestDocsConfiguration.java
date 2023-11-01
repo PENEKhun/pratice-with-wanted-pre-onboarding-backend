db6dbd840193f43be4000bede9eb1932c72d6047
penekhun <penekhun@gmail.com>
2023-11-01 22:09:30 +0900

    docs(restdocs): RestDocs 요청/응답 전처리를 전역으로 수행하겠끔 수정

---
+++ src/test/java/org/penekhun/wanted2023/global/docs/RestDocsConfiguration.java
@@ -0,0 +1,33 @@
+package org.penekhun.wanted2023.global.docs;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+
+
+@TestConfiguration
+public class RestDocsConfiguration {
+
+  @Bean
+  public RestDocumentationResultHandler restDocumentationResultHandler() {
+    return MockMvcRestDocumentation.document(
+        "{class-name}/{method-name}",
+        preprocessRequest(
+            modifyHeaders().remove("Content-Length"),
+            modifyHeaders().remove("Host"),
+            prettyPrint()
+        ),
+        preprocessResponse(
+            modifyHeaders().remove("Content-Length"),
+            prettyPrint()
+        )
+    );
+  }
+
+}
