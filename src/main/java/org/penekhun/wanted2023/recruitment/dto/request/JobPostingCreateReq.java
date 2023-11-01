eb3bd7eff7ebcfc58f2de513788fba6f41fdab58
penekhun <penekhun@gmail.com>
2023-11-01 22:10:31 +0900

    feat: Record json 직렬화를 위한 어노테이션 설정 추가
    컨트롤러 test 시 record 객체를 json으로 변환하여 요청을 날려주기 위해서 필요함.

--- src/main/java/org/penekhun/wanted2023/recruitment/dto/request/JobPostingCreateReq.java
+++ src/main/java/org/penekhun/wanted2023/recruitment/dto/request/JobPostingCreateReq.java
@@ -1,10 +1,14 @@
 package org.penekhun.wanted2023.recruitment.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@JsonSerialize
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record JobPostingCreateReq(
     @Min(value = 0, message = "채용 보상금은 0원 이상이어야 합니다.")
     @Max(value = 100000000, message = "채용 보상금은 1억원 이하로 입력해주세요.")
