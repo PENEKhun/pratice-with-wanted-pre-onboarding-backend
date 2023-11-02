bd57777efd5146e9aaeed01726d6ebc12ef0f53f
penekhun <penekhun@gmail.com>
2023-11-02 19:54:07 +0900

    refactor: 코드 이동

--- src/main/java/org/penekhun/wanted2023/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/penekhun/wanted2023/global/exception/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package org.penekhun.wanted2023.global.exception;
 
 import org.penekhun.wanted2023.global.rest_controller.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -19,6 +23,13 @@ public class GlobalExceptionHandler {
                 err.getMessage(),
                 null)
         );
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(BindException.class)
+  public ApiResponse<?> handleBindException(BindException e) {
+    ObjectError firstErr = e.getBindingResult().getAllErrors().get(0);
+    return ApiResponse.of(HttpStatus.BAD_REQUEST, firstErr.getDefaultMessage());
   }
 
 }
