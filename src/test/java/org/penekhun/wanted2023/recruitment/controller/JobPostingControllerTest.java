90441865652753637210d9832bc4dd063fb5f428
penekhun <penekhun@gmail.com>
2023-11-02 23:41:39 +0900

    test(controller): 채용공고 삭제 테스트 작성

--- src/test/java/org/penekhun/wanted2023/recruitment/controller/JobPostingControllerTest.java
+++ src/test/java/org/penekhun/wanted2023/recruitment/controller/JobPostingControllerTest.java
@@ -5,6 +5,8 @@ import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 @WebMvcTest(controllers = JobPostingController.class)
@@ -88,98 +91,21 @@ class JobPostingControllerTest extends RestDocsSupport {
         .andReturn();
   }
 
-      @TestWithEnterpriseAccount
-      @DisplayName("채용 보상금이 음수라면, 400 응답을 받습니다.")
-      void reward_valid_err() throws Exception {
-        // given
-        String given = "-1";
-        Map<String, String> input = new HashMap<>();
-        input.put("recruitPosition", "개발자");
-        input.put("recruitReward", given);
-        input.put("description", "개발자를 모집합니다.");
 
-        // when & then
-        mockMvc
-            .perform(
-                post("/api/v1/job-posting")
-                    .with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(input)))
-            .andExpect(status().isBadRequest());
-      }
-
-      @TestWithEnterpriseAccount
-      @DisplayName("채용 포지션이 null 이라면, 400 응답을 받습니다.")
-      void position_null_valid_err() throws Exception {
-        // given
-        String given = null;
-        Map<String, String> input = new HashMap<>();
-        input.put("recruitPosition", given);
-        input.put("recruitReward", "10000");
-        input.put("description", "개발자를 모집합니다.");
-
-        // when & then
-        mockMvc
-            .perform(
-                post("/api/v1/job-posting")
-                    .with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(input)))
-            .andDo(print())
-            .andExpectAll(
-                status().isBadRequest(),
-                jsonPath("$.data").value("채용 포지션을 입력해주세요."));
-      }
-
-      @TestWithEnterpriseAccount
-      @DisplayName("채용 포지션이 길다면, 400 응답을 받습니다.")
-      void position_len_valid_err() throws Exception {
-        // given
-        String given = "a".repeat(31);
-        Map<String, String> input = new HashMap<>();
-        input.put("recruitPosition", given);
-        input.put("recruitReward", "10000");
-        input.put("description", "개발자를 모집합니다.");
-
-        // when & then
-        mockMvc
-            .perform(
-                post("/api/v1/job-posting")
-                    .with(csrf())
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(input)))
-            .andExpectAll(
-                status().isBadRequest(),
-                jsonPath("$.data").value("채용 포지션은 30자 이하로 입력해주세요."));
-      }
-    }
-  }
-
-  @Nested
-  @Disabled
-  @DisplayName("채용 공고 삭제시에")
-  class delete_job_posting {
-
-    @Nested
-    @DisplayName("개인 계정은")
-    class with_personal_account {
-
-      @TestWithPersonalAccount
-      @DisplayName("401 응답을 받습니다.")
-      void personal_cant_delete_jobPosting() throws Exception {
-        mockMvc
-            .perform(
-                post("/api/v1/job-posting/1")
-                    .with(csrf()))
-            .andExpect(status().isBadRequest());
-      }
-
-    }
-
-    @Nested
-    @DisplayName("기업 계정은")
-    class with_enterprise_account {
-
-    }
+  @Test
+  @DisplayName("채용 공고 삭제에 성공한다")
+  void delete_job_posting() throws Exception {
+    mockMvc
+        .perform(
+            RestDocumentationRequestBuilders
+                .delete("/api/v1/job-posting/{jobPostId}", 1L)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(
+            restDocs.document(
+                pathParameters(
+                    parameterWithName("jobPostId")
+                        .description("삭제하려는 채용 공고 id"))
+            ));
   }
 }
