0b9e1dceceb4ad653c22a0fe2610f0b4fb6da4e8
penekhun <penekhun@gmail.com>
2023-11-06 19:30:42 +0900

    test(integration): 채용 공고 생성 테스트에 픽스쳐 적용

--- src/test/java/org/penekhun/wanted2023/recruitment/service/JobPostingServiceTest.java
+++ src/test/java/org/penekhun/wanted2023/recruitment/service/JobPostingServiceTest.java
@@ -1,20 +1,28 @@
 package org.penekhun.wanted2023.recruitment.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.penekhun.wanted2023.recruitment.fixture.RecruitmentFixture.CreateJobPosting;
+import static org.penekhun.wanted2023.recruitment.fixture.RecruitmentFixture.CreateJobPostingReq;
+import static org.penekhun.wanted2023.user.UserFixture.CreateEnterpriseUserAccount;
 
-import com.navercorp.fixturemonkey.FixtureMonkey;
-import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
-import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.penekhun.wanted2023.recruitment.dto.request.JobPostingCreateReq;
 import org.penekhun.wanted2023.recruitment.dto.response.JobPostingCreateRes;
+import org.penekhun.wanted2023.recruitment.dto.response.JobPostingSearchRes;
+import org.penekhun.wanted2023.recruitment.entity.JobPosting;
+import org.penekhun.wanted2023.recruitment.repository.JobPostingRepository;
 import org.penekhun.wanted2023.user.entity.EnterpriseUserAccount;
 import org.penekhun.wanted2023.user.repository.EnterpriseAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -22,13 +30,12 @@ import org.springframework.transaction.annotation.Transactional;
 @DisplayName("JobPostingService 테스트")
 class JobPostingServiceTest {
 
-  final EnterpriseUserAccount enterpriseAccount = EnterpriseUserAccount.builder()
-      .username("samsung")
-      .password("1234")
-      .build();
 
   @Autowired
   JobPostingService jobPostingService;
+
+  @Autowired
+  JobPostingRepository jobPostingRepository;
 
   @Autowired
   EnterpriseAccountRepository enterpriseAccountRepository;
@@ -42,23 +49,31 @@ class JobPostingServiceTest {
   @DisplayName("createJobPosting 메서드는")
   class createJobPosting {
 
+    EnterpriseUserAccount enterpriseAccount = CreateEnterpriseUserAccount();
+
+    @BeforeEach
+    void setUp() {
+      enterpriseAccountRepository.save(enterpriseAccount);
+    }
+
+    @AfterEach
+    void tearDown() {
+      enterpriseAccountRepository.delete(enterpriseAccount);
+    }
+
     @RepeatedTest(100)
     @DisplayName("모든 인자가 정상일때 정상적으로 채용 공고를 생성합니다.")
     void happyCase() {
       // given
-      FixtureMonkey fixture = FixtureMonkey.builder()
-          .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
-          .plugin(new JakartaValidationPlugin())
-          .build();
-
-      JobPostingCreateReq arg = fixture.giveMeOne(JobPostingCreateReq.class);
+      JobPostingCreateReq arg = CreateJobPostingReq();
 
       // when
       JobPostingCreateRes response = jobPostingService.createJobPosting(enterpriseAccount, arg);
 
       // then
       assertThat(response)
-          .as("id", "recruitReward", "recruitPosition", "description").hasNoNullFieldsOrProperties()
+          .as("id", "recruitReward", "recruitPosition", "description")
+          .hasNoNullFieldsOrProperties()
           .extracting(
               "recruitReward",
               "recruitPosition",
