1d2b5dcfb24bbbabac0f7fbe9d37cf64e7800f50
penekhun <penekhun@gmail.com>
2023-11-06 19:29:25 +0900

    test: 채용공고 관련 테스트 픽스쳐 생성

---
+++ src/test/java/org/penekhun/wanted2023/recruitment/fixture/RecruitmentFixture.java
@@ -0,0 +1,39 @@
+package org.penekhun.wanted2023.recruitment.fixture;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
+import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
+import net.jqwik.api.Arbitraries;
+import org.penekhun.wanted2023.recruitment.dto.request.JobPostingCreateReq;
+import org.penekhun.wanted2023.recruitment.entity.JobPosting;
+
+public class RecruitmentFixture {
+
+  public static JobPosting CreateJobPosting() {
+    FixtureMonkey fixture = FixtureMonkey.builder()
+        .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+        .build();
+
+    return fixture.giveMeBuilder(JobPosting.class)
+        .set("recruitReward",
+            Arbitraries.integers().between(0, 100000000).sample())
+        .set("recruitPosition",
+            Arbitraries.strings().withCharRange('a', 'z').ofMinLength(1).ofMaxLength(30))
+        .set("description",
+            Arbitraries.strings().withCharRange('a', 'z').ofMinLength(1).ofMaxLength(10000))
+        .set("requiredSkill",
+            Arbitraries.strings().withCharRange('a', 'z').ofMinLength(4).ofMaxLength(20))
+        .build().sample();
+  }
+
+  public static JobPostingCreateReq CreateJobPostingReq() {
+
+    FixtureMonkey fixture = FixtureMonkey.builder()
+        .objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+        .plugin(new JakartaValidationPlugin())
+        .build();
+
+    return fixture.giveMeOne(JobPostingCreateReq.class);
+  }
+
+}
