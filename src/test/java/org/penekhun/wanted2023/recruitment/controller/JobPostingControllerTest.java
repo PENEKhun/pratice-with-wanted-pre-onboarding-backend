5e04f522c76adcb8cd005dfb45c0bd16355622d2
penekhun <penekhun@gmail.com>
2023-11-07 12:19:48 +0900

    style: remove unused import

--- src/test/java/org/penekhun/wanted2023/recruitment/controller/JobPostingControllerTest.java
+++ src/test/java/org/penekhun/wanted2023/recruitment/controller/JobPostingControllerTest.java
@@ -8,7 +8,6 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.requestF
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
