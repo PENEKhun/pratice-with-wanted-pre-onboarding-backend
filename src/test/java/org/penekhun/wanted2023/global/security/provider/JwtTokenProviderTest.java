dac35c77dfa78bf43c5f1180acab67b8cabd1971
penekhun <penekhun@gmail.com>
2023-11-29 15:16:21 +0900

    test: 누락된 테스트 클래스 생성

---
+++ src/test/java/org/penekhun/wanted2023/global/security/provider/JwtTokenProviderTest.java
@@ -0,0 +1,22 @@
+package org.penekhun.wanted2023.global.security.provider;
+
+import org.junit.jupiter.api.Test;
+
+class JwtTokenProviderTest {
+
+  @Test
+  void afterPropertiesSet() {
+  }
+
+  @Test
+  void createToken() {
+  }
+
+  @Test
+  void getAuthentication() {
+  }
+
+  @Test
+  void validateToken() {
+  }
+}
